98cd32ced69c5d59350e1ec96c128ad1184d96db
Brian <bdomercial@me.com>
2019-08-05 22:05:51 -0700

    added useEffect to target ref scrollTarget and keep the text entry always visible. Added in quick replies. TODO: styles need to be fixed!

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -15,6 +15,7 @@ const ConversationContainer = styled.div`
     .incoming,
     .outgoing {
       display: flex;
+      flex-direction: column;
       line-height: 2rem;
       list-style: none;
       position: relative;
@@ -28,6 +29,9 @@ const ConversationContainer = styled.div`
     }
     .incoming {
       justify-content: flex-start;
+      .button-container {
+        margin: .8rem 0 .8rem 3.6rem;
+      }
       .bubble {
         background-color: #eeeeee;
         margin: 0 0 0.2rem 3.6rem;
@@ -87,11 +91,28 @@ const ConversationContainer = styled.div`
         }
       }
     }
+    .button-container {
+      display: flex;
+      flex-direction: row;
+      .quickReply {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 2px solid blue;
+        height: 4rem;
+        border-radius: 2rem;
+        font-size: 1.4rem;
+        background-color: #ffffff;
+        min-width: 6rem;
+        margin-right: .8rem;
+      }
+    }
   }
   .sendMessage {
     display: flex;
     border-top: 1px solid #eeeeee;
     padding: 0.8rem;
+    background-color: #ffffff;
     input {
       font-size: 1.4rem;
       padding: 0.8rem;
@@ -111,53 +132,73 @@ let currentMsg = 0;
 let currentKey = 100;
 
 export default function Conversation() {
+
+  const scrollTarget = React.createRef();
+
+  useEffect(() => {
+    scrollTarget && scrollTarget.current.scrollIntoView({ behavior: "smooth" });
+    // elem.scrollIntoView(false);
+    // elem.scrollIntoView({ block: "end" });
+    // scrollTarget.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+  });
+
   const questions = [
     {
       index: 3,
       content: "Did you reach someone with authority to approve the request?",
-      type: "incoming"
+      type: "incoming",
+      quickReplies: ["Yes", "No"]
     },
     {
       index: 4,
       content: "What is the name of the person you reached?",
-      type: "incoming"
+      type: "incoming",
+      quickReplies: ["Anje Keizer", "I don't know"]
     },
     {
       index: 5,
       content: "What is the title of the person you reached?",
-      type: "incoming"
+      type: "incoming",
+      quickReplies: []
     },
     {
       index: 6,
       content: "Did they approve the customer's request?",
-      type: "incoming"
+      type: "incoming",
+      quickReplies: ["Yes", "No", "Partially"]
     },
     {
       index: 7,
       content:
         "Great! I will send the customer an email and let them know that a full refund is on the way. Go ahead and close this tab – you don't need to do anything more.",
-      type: "incoming"
+      type: "incoming",
+      quickReplies: []
     }
   ];
+
   const [input, setInput] = useState("");
+
   const [messages, setMessages] = useState([
     {
       index: 0,
       content:
         "What happened: Chachi Arredondo called on Aug 4, 2019 to cancel their stay at Hotel Theodore, Seattle during Aug 11, 2019 - Aug 14, 2019, due to medical emergency.  (Itinerary: 1234567890123)",
-      type: "incoming"
+      type: "incoming",
+      quickReplies: []
     },
     {
       index: 1,
       content:
         "What to do: Call the Hotel Theodore, Seattle and ask Anje Keizer, General Manager to approve the waiver for this cancellation.",
-      type: "incoming"
+      type: "incoming",
+      quickReplies: []
     },
     {
       index: 2,
       content:
         "Please note: This is the first out of 3 attempts to get the waiver",
-      type: "incoming"
+      type: "incoming",
+      quickReplies: ["Call hotel"]
     }
   ]);
 
@@ -165,24 +206,24 @@ export default function Conversation() {
     <>
       <div className={msg.type} key={msg.index}>
         <div className="bubble">{msg.content}</div>
+        {msg.quickReplies.length > 0 && <div className="button-container">
+          {msg.quickReplies.map(reply => (
+            <button className="quickReply">{reply}</button>
+          ))}
+          </div>}
       </div>
-      <div classNeme="scrollIntoView"></div>
     </>
   ));
-
-  const scrollTo = document.getElementsByClassName("scrollIntoView");
 
   const handleChange = e => {
     return setInput(e.target.value);
   };
 
   const nextQuestion = () => {
-    setMessages(prevMessages => [...prevMessages, questions[currentMsg]]);
-    currentMsg++;
-    scrollTo.scrollIntoView({
-      block: 'end',
-      behavior: 'smooth'
-    });
+    if(currentMsg < questions.length + 2) {
+      setMessages(prevMessages => [...prevMessages, questions[currentMsg]]);
+      currentMsg++;
+    }
   };
 
   const handleSubmit = e => {
@@ -194,17 +235,9 @@ export default function Conversation() {
       currentKey++;
       setInput("");
       setTimeout(nextQuestion, 500);
-      scrollTo.scrollIntoView({
-        block: 'end',
-        behavior: 'smooth'
-      });
     }
   };
-
-  //TODO investigate useEffect to make sure that the messages array is updated
-  // useEffect(() => {
-  //   nextQuestion();
-  // }, [messages]);
+  
 
   return (
     <ConversationContainer>
@@ -215,6 +248,7 @@ export default function Conversation() {
         <input type="text" onChange={handleChange} value={input} />
         <button type="submit">Send</button>
       </form>
+      <div ref={scrollTarget} />
     </ConversationContainer>
   );
 }
